ea4fa2dadb07df50eefd46e340172138a3c35329
naro83SH <nareksh777@gmail.com>
2025-06-27 10:26:46 +0200

    Now Items and Blocks have ther tabs in creative !

--- src/main/java/net/naro/testmod/TestMod.java
+++ src/main/java/net/naro/testmod/TestMod.java
@@ -3,18 +3,20 @@ package net.naro.testmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.naro.testmod.block.ModBlocks;
+import net.naro.testmod.item.ModItemGroups;
 import net.naro.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //I'm happy my 1st import finaly work !
-
 public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerModItems();
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
