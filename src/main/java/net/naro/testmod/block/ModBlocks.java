c78f0baa2d6fc88097aed47c918ea7b442078019
naro83SH <nareksh777@gmail.com>
2025-06-27 13:25:20 +0200

    List of add : -Blocks Drops -Recipes

--- src/main/java/net/naro/testmod/block/ModBlocks.java
+++ src/main/java/net/naro/testmod/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.naro.testmod.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.naro.testmod.TestMod;
 
 public class ModBlocks {
@@ -17,15 +19,24 @@ public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(1f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
-
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(1f)
                     .requiresTool()));
 
+public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+        new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                AbstractBlock.Settings.create().strength(1f).requiresTool()));
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(1f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, Identifier.of(TestMod.MOD_ID, name), block);
+
     }
+
 
     private static void registerBlockItem(String name, Block block) {
         Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name),
