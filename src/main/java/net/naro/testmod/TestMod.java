505f4a47af6f8239ddc41a121bf9df653f3ce208
naro83SH <nareksh777@gmail.com>
2025-06-27 09:54:02 +0200

    NEW Blocks : Block of Raw Pink Garnet and Block of Pink Garnet !

--- src/main/java/net/naro/testmod/TestMod.java
+++ src/main/java/net/naro/testmod/TestMod.java
@@ -2,6 +2,7 @@ package net.naro.testmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.naro.testmod.block.ModBlocks;
 import net.naro.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,5 +16,6 @@ public class TestMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/naro/testmod/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.naro.testmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+import net.naro.testmod.TestMod;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
+
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TestMod.LOGGER.info("Registering Mod Item for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
